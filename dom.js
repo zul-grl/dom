11a07c81751304d0cde0d9295894ed91b9dde1ac
24LP7683 24LP7683 <24LP7683@admins-iMac.local>
2024-12-06 12:34:15 +0800

    dom

---
+++ dom.js
@@ -0,0 +1,77 @@
+let parentdiv = document.getElementById("parent");
+let cardobj = [
+  {
+    price: "482 сая ₮",
+    address: "1 минутын өмнө  |  УБ — Хан-Уул, Хурд",
+    title: "Рапид харш хороололд 125 мкв 4 өрөө байр худалдана",
+    ImgUrl:
+      "https://cdn1.unegui.mn/media/cache1/8d/0a/8d0a5cc9c74992f053bab7c79d430a63.webp",
+  },
+  {
+    price: "499,900 ₮",
+    address: "1 минутын өмнө  |  УБ — Баянзүрх, 14-р хороолол",
+    title: " Gantel",
+    ImgUrl:
+      "https://cdn1.unegui.mn/media/cache1/8d/0a/8d0a5cc9c74992f053bab7c79d430a63.webp",
+  },
+  {
+    price: "2.2 сая ₮",
+    address: "  1 минутын өмнө  |  УБ — Хан-Уул, Зайсан",
+    title: "Зайсан, рома таунд 110мкв 3 өрөө орон сууц",
+    ImgUrl:
+      "https://cdn1.unegui.mn/media/cache1/8d/0a/8d0a5cc9c74992f053bab7c79d430a63.webp",
+  },
+  {
+    price: "200 сая ₮",
+    address: "1 минутын өмнө  |  УБ — Хан-Уул, Хан-Уул, Хороо 2",
+    title: "ХУД 2-р хороо гоёлын ард 3 өрөө 67.99м2 байр",
+    ImgUrl:
+      "https://cdn1.unegui.mn/media/cache1/8d/0a/8d0a5cc9c74992f053bab7c79d430a63.webp",
+  },
+  {
+    price: "482 сая ₮",
+    address: "1 минутын өмнө  |  УБ — Хан-Уул, Хурд",
+    title: "Рапид харш хороололд 125 мкв 4 өрөө байр худалдана",
+    ImgUrl:
+      "https://cdn1.unegui.mn/media/cache1/8d/0a/8d0a5cc9c74992f053bab7c79d430a63.webp",
+  },
+  {
+    price: "499,900 ₮",
+    address: "1 минутын өмнө  |  УБ — Баянзүрх, 14-р хороолол",
+    title: " Gantel",
+    ImgUrl:
+      "https://cdn1.unegui.mn/media/cache1/8d/0a/8d0a5cc9c74992f053bab7c79d430a63.webp",
+  },
+  {
+    price: "2.2 сая ₮",
+    address: "  1 минутын өмнө  |  УБ — Хан-Уул, Зайсан",
+    title: "Зайсан, рома таунд 110мкв 3 өрөө орон сууц",
+    ImgUrl:
+      "https://cdn1.unegui.mn/media/cache1/8d/0a/8d0a5cc9c74992f053bab7c79d430a63.webp",
+  },
+  {
+    price: "200 сая ₮",
+    address: "1 минутын өмнө  |  УБ — Хан-Уул, Хан-Уул, Хороо 2",
+    title: "ХУД 2-р хороо гоёлын ард 3 өрөө 67.99м2 байр",
+    ImgUrl:
+      "https://cdn1.unegui.mn/media/cache1/8d/0a/8d0a5cc9c74992f053bab7c79d430a63.webp",
+  },
+  {
+    price: "2.2 сая ₮",
+    address: "  1 минутын өмнө  |  УБ — Хан-Уул, Зайсан",
+    title: "Зайсан, рома таунд 110мкв 3 өрөө орон сууц",
+    ImgUrl:
+      "https://cdn1.unegui.mn/media/cache1/8d/0a/8d0a5cc9c74992f053bab7c79d430a63.webp",
+  },
+  {
+    price: "200 сая ₮",
+    address: "1 минутын өмнө  |  УБ — Хан-Уул, Хан-Уул, Хороо 2",
+    title: "ХУД 2-р хороо гоёлын ард 3 өрөө 67.99м2 байр",
+    ImgUrl:
+      "https://cdn1.unegui.mn/media/cache1/8d/0a/8d0a5cc9c74992f053bab7c79d430a63.webp",
+  },
+];
+
+
+
+parentdiv.innerHTML = card;
